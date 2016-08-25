8d09d9012cf2f404e05b131746fff530b5ba1c50
fakih <fakih.arief@tokopedia.com>
2016-08-25 21:40:39 +0700

    add pdf

--- app.go
+++ app.go
@@ -1,7 +1,163 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	tes "github.com/fakihariefnoto/gowebservice/test"
+	"github.com/jung-kurt/gofpdf"
+)
+
+func init() {
+	//cleanup()
+}
+
+func cleanup() {
+	filepath.Walk(tes.PdfDir(),
+		func(path string, info os.FileInfo, err error) (reterr error) {
+			if info.Mode().IsRegular() {
+				dir, _ := filepath.Split(path)
+				if "reference" != filepath.Base(dir) {
+					if len(path) > 3 {
+						if path[len(path)-4:] == ".pdf" {
+							os.Remove(path)
+						}
+					}
+				}
+			}
+			return
+		})
+}
+
+type fontResourceType struct {
+}
+
+func (f fontResourceType) Open(name string) (rdr io.Reader, err error) {
+	var buf []byte
+	buf, err = ioutil.ReadFile(tes.FontFile(name))
+	if err == nil {
+		rdr = bytes.NewReader(buf)
+		fmt.Printf("Generalized font loader reading %s\n", name)
+	}
+	return
+}
 
 func main() {
 	fmt.Println("Hello, it's just first test")
+	ExampleFpdf_MultiCell()
+}
+
+func ExampleFpdf_AddPage() {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.SetHeaderFunc(func() {
+		pdf.Image(tes.ImageFile("logo.png"), 10, 6, 30, 0, false, "", 0, "")
+		pdf.SetY(5)
+		pdf.SetFont("Arial", "B", 15)
+		pdf.Cell(80, 0, "")
+		pdf.CellFormat(30, 10, "Title", "1", 0, "C", false, 0, "")
+		pdf.Ln(20)
+	})
+	pdf.SetFooterFunc(func() {
+		pdf.SetY(-15)
+		pdf.SetFont("Arial", "I", 8)
+		pdf.CellFormat(0, 10, fmt.Sprintf("Page %d/{nb}", pdf.PageNo()),
+			"", 0, "C", false, 0, "")
+	})
+	pdf.AliasNbPages("")
+	pdf.AddPage()
+	pdf.SetFont("Times", "", 12)
+	for j := 1; j <= 40; j++ {
+		pdf.CellFormat(0, 10, fmt.Sprintf("Printing line number %d", j),
+			"", 1, "", false, 0, "")
+	}
+	fileStr := tes.Filename("Fpdf_AddPage")
+	err := pdf.OutputFileAndClose(fileStr)
+	tes.Summary(err, fileStr)
+	// Output:
+	// Successfully generated pdf/Fpdf_AddPage.pdf
+}
+
+// This example demonstrates word-wrapping, line justification and
+// page-breaking.
+func ExampleFpdf_MultiCell() {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	titleStr := "20000 Leagues Under the Seas"
+	pwd, errs := os.Getwd()
+	if errs != nil {
+		fmt.Println(errs)
+		os.Exit(1)
+	}
+	pdf.SetTitle(titleStr, false)
+	pdf.SetAuthor("Jules Verne", false)
+	pdf.SetHeaderFunc(func() {
+		// Arial bold 15
+		pdf.SetFont("Arial", "B", 15)
+		// Calculate width of title and position
+		wd := pdf.GetStringWidth(titleStr) + 6
+		pdf.SetX((210 - wd) / 2)
+		// Colors of frame, background and text
+		pdf.SetDrawColor(0, 80, 180)
+		pdf.SetFillColor(230, 230, 0)
+		pdf.SetTextColor(220, 50, 50)
+		// Thickness of frame (1 mm)
+		pdf.SetLineWidth(1)
+		// Title
+		pdf.CellFormat(wd, 9, titleStr, "1", 1, "C", true, 0, "")
+		// Line break
+		pdf.Ln(10)
+	})
+	pdf.SetFooterFunc(func() {
+		// Position at 1.5 cm from bottom
+		pdf.SetY(-15)
+		// Arial italic 8
+		pdf.SetFont("Arial", "I", 8)
+		// Text color in gray
+		pdf.SetTextColor(128, 128, 128)
+		// Page number
+		pdf.CellFormat(0, 10, fmt.Sprintf("Page %d", pdf.PageNo()),
+			"", 0, "C", false, 0, "")
+	})
+	chapterTitle := func(chapNum int, titleStr string) {
+		// 	// Arial 12
+		pdf.SetFont("Arial", "", 12)
+		// Background color
+		pdf.SetFillColor(200, 220, 255)
+		// Title
+		pdf.CellFormat(0, 6, fmt.Sprintf("Chapter %d : %s", chapNum, titleStr),
+			"", 1, "L", true, 0, "")
+		// Line break
+		pdf.Ln(4)
+	}
+	chapterBody := func(fileStr string) {
+		// Read text file
+		txtStr, err := ioutil.ReadFile(fileStr)
+		if err != nil {
+			pdf.SetError(err)
+		}
+		// Times 12
+		pdf.SetFont("Times", "", 12)
+		// Output justified text
+		pdf.MultiCell(0, 5, string(txtStr), "", "", false)
+		// Line break
+		pdf.Ln(-1)
+		// Mention in italics
+		pdf.SetFont("", "I", 0)
+		pdf.Cell(0, 5, "(end of excerpt)")
+	}
+	printChapter := func(chapNum int, titleStr, fileStr string) {
+		pdf.AddPage()
+		chapterTitle(chapNum, titleStr)
+		chapterBody(fileStr)
+	}
+	printChapter(1, "A RUNAWAY REEF", pwd+"/isian.txt") //tes.TextFile("isian.txt"))
+	printChapter(2, "THE PROS AND CONS", pwd+"/isian.txt")
+	fileStr := pwd + "nyoba_Fpdf_MultiCell.pdf"
+	err := pdf.OutputFileAndClose(fileStr)
+	tes.Summary(err, fileStr)
+	// Output:
+	// Successfully generated pdf/Fpdf_MultiCell.pdf
 }
